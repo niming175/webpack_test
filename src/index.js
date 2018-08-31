5d56f6ea86ed3f22ba37a463b56ffb6f9e5f78c0
niming175 <niming175@qq.com>
2018-08-31 19:47:05 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import img from './1.jpg'
 
 function component () {
   var element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], '');
+  element.classList.add('hello');
+
+  // 将图片添加到div
+  let myImg = new Image();
+  myImg.src = img;
+
+  element.appendChild(myImg);
+
   return element;
 }
 
